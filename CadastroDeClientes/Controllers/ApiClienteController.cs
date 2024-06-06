61def2f2e722a3c142ff6678d010c55d2be34250
unknown <art.rib@hotmail.com>
2024-06-06 19:17:23 -0300

    Inicio de implementacao de API

---
+++ CadastroDeClientes/Controllers/ApiClienteController.cs
@@ -0,0 +1,49 @@
+﻿using CadastroDeClientes.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroDeClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiClienteController : ControllerBase
+    {
+        public List<ClienteModel> ListarTodos()
+        {
+
+        }
+
+        public ClienteModel BuscarPorId(int id)
+        {
+
+        }
+        public ClienteModel BuscarPorCodigo(String codigo)
+        {
+
+        }
+        public ClienteModel BuscarPorDocumento(String documento)
+        {
+
+        }
+        public ClienteModel BuscarPorNomeRazaoSocial(String NomeRazaoSocial)
+        {
+
+        }
+
+        public ClienteModel Criar(ClienteModel cliente)
+        {
+
+        }
+
+        public ClienteModel Alterar(ClienteModel cliente)
+        {
+
+        }
+
+        public bool Deletar(int id)
+        {
+            return true;
+        }
+        
+    }
+}
